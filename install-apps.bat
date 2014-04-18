e3e7933eeb367dc95077ad72964121a08ecee2d6
Rodrigo Ramos dos Santos <rodrigo.ramos.santos@gmail.com>
2014-04-18 18:54:17 -0300

    Separacao dos apps em categorias

--- install-apps.bat
+++ install-apps.bat
@@ -1,31 +1,46 @@
-cinst ConEmu
-cinst notepadplusplus
-cinst Firefox
-cinst vim
-cinst bitdefenderavfree
-cinst dropbox
-cinst ccleaner
-cinst gimp
-cinst malwarebytes
-cinst 7zip
-cinst fiddler4
-cinst GoogleChrome
-cinst virtualbox
-cinst PDFCreator
-cinst sumatrapdf.install
-cinst sublimetext2
-cinst vlc
-cinst winmerge
-cinst pencil
-cinst SourceCodePro
-cinst webpi
-cinst launchy
-cinst git
-cinst lockhunter
-cinst nodejs.install
-cinst cdburnerxp
-cinst windirstat
-cinst linqpad4
-cinst recuva
-cinst HxD
-cinst greenshot 
+@rem echo "%1"
+@set %installUtil=0
+@set %installImage=0
+@set %installDev=0
+
+@if "%1"=="" (
+	goto usage
+)
+
+@if /I "%1%"=="util" (
+	%installUtil% = 1
+)
+@if /I "%1%"=="image" (
+	%installImage% = 1
+)
+@if /I "%1%"=="dev" (
+	%installDev% = 1
+)
+@if /I "%1%"=="all" (
+	%installUtil% = 1
+	%installImage%=1
+	%installDev%=1
+)
+@if "%installUtil%"=="1" (
+	start cinst notepadplusplus ccleaner 7zip Firefox GoogleChrome sumatrapdf.install vlc launchy recuva greenshot dropbox malwarebytes cdburnerxp bitdefenderavfree PDFCreator winmerge 
+)
+
+@if "%installImage%"=="1" (
+	start cinst gimp
+)
+
+@if "%installDev%"=="1" (
+	start cinst Console2 vim fiddler4 virtualbox sublimetext2 pencil SourceCodePro webpi git lockhunternodejs.install windirstat linqpad4 HxD
+)
+:usage
+@echo.
+@echo Usage: 
+@echo install-apps category
+@echo.
+@echo Example: install-apps all
+@echo.
+@echo Avaliabe categories:
+@echo       all                All categories
+@echo       dev                Only development apps
+@echo       image              Only image apps
+@echo       utils              Only util apps
