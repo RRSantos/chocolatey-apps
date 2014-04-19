9a1ffca5a84901f1984eecc2fba3c4274e88ead6
Rodrigo Ramos dos Santos <rodrigo.ramos.santos@gmail.com>
2014-04-19 09:46:45 -0300

    change start with call

--- install-apps.bat
+++ install-apps.bat
@@ -1,37 +1,45 @@
-@rem echo "%1"
-@set %installUtil=0
-@set %installImage=0
-@set %installDev=0
+@set installUtil=0
+@set installImage=0
+@set installDev=0
+@set paramOK=0
 
 @if "%1"=="" (
 	goto usage
 )
 
 @if /I "%1%"=="util" (
-	%installUtil% = 1
+	@set installUtil=1
+	@set paramOk=1
 )
-@if /I "%1%"=="image" (
-	%installImage% = 1
+@if /I "%1"=="image" (
+	@set installImage=1
+	@set paramOk=1
 )
-@if /I "%1%"=="dev" (
-	%installDev% = 1
+@if /I "%1"=="dev" (
+	@set installDev=1
+	@set paramOk=1
 )
-@if /I "%1%"=="all" (
-	%installUtil% = 1
-	%installImage%=1
-	%installDev%=1
+@if /I "%1"=="all" (
+	@set installUtil=1
+	@set installImage=1
+	@set installDev=1
+	@set paramOk=1
 )
 @if "%installUtil%"=="1" (
-	start cinst notepadplusplus ccleaner 7zip Firefox GoogleChrome sumatrapdf.install vlc launchy recuva greenshot dropbox malwarebytes cdburnerxp bitdefenderavfree PDFCreator winmerge 
+	call cinst notepadplusplus ccleaner 7zip Firefox GoogleChrome sumatrapdf.install vlc launchy recuva greenshot dropbox malwarebytes cdburnerxp bitdefenderavfree PDFCreator winmerge 
 )
 
 @if "%installImage%"=="1" (
-	start cinst gimp
+	call cinst gimp
 )
 
 @if "%installDev%"=="1" (
-	start cinst Console2 vim fiddler4 virtualbox sublimetext2 pencil SourceCodePro webpi git lockhunternodejs.install windirstat linqpad4 HxD
+	call cinst Console2 vim fiddler4 virtualbox sublimetext2 pencil SourceCodePro webpi git lockhunternodejs.install windirstat linqpad4 HxD
 )
+@if "%paramOk%"=="1" (
+	goto end
+)
+
 :usage
 @echo.
 @echo Usage: 
@@ -44,3 +52,5 @@
 @echo       dev                Only development apps
 @echo       image              Only image apps
 @echo       utils              Only util apps
+
+:end
